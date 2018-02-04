1d3547553800d3c9bee4b1fa7d5530d124091daa
Matt Lam <mwklam@hawaii.edu>
2018-02-03 19:44:57 -1000

    write four functions to sum list

---
+++ sumofnumbers.js
@@ -0,0 +1,31 @@
+function sumFor(list){
+  let sum = 0;
+  for(var i=0; i<list.length;i++){
+    sum += list[i];
+  }
+  return sum;
+}
+
+function sumWhile(list){
+  let sum = 0;
+  var i = 0;
+  while(i<list.length){
+    sum+=list[i];
+    i++;
+  }
+  return sum;
+}
+
+function sumRecursion(list){
+  if(list.length>1){
+    list[0]+=list.pop();
+    return sumRecur(list);
+  }
+  else{
+    return list[0];
+  }
+}
+
+function sumTheSimpleWay(list){
+  return _.reduce([1, 2, 3], function(memo, num){ return memo + num; }, 0);
+}
